e7daa22a9d50b55fa6ae7a9b74ca4c8c983fb89d
Eliah Reeves <ereeclimb@gmail.com>
2025-07-28 18:17:17 -0700

    104

--- nixosModules/default.nix
+++ nixosModules/default.nix
@@ -6,10 +6,12 @@
     ./amd-utils.nix
     ./system76-utils.nix
     ./power-button-sleep.nix
+    ./nix-ld.nix
   ];
   system76-utils.enable = lib.mkDefault false;
   capslock-arrow-keys.enable = lib.mkDefault true;
   amd-utils.enable = lib.mkDefault false;
   hyprland.enable = lib.mkDefault true;
   power-button-sleep.enable = lib.mkDefault false;
+  nix-ld.enable = lib.mkDefault true;
 }
---
+++ nixosModules/nix-ld.nix
@@ -0,0 +1,27 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
+  options = {
+    nix-ld.enable = lib.mkEnableOption "Enable nix-ld for running unpatched dynamic binaries";
+  };
+  config = lib.mkIf config.nix-ld.enable {
+    programs.nix-ld = {
+      enable = true;
+      libraries = with pkgs; [
+        # Common libraries that many dynamic binaries expect
+        stdenv.cc.cc
+        zlib
+        fuse3
+        icu
+        nss
+        openssl
+        curl
+        expat
+        # Add more libraries as needed
+      ];
+    };
+  };
+}
