4ac6133439583535baed024943f503796f6fc5d8
Eliah Reeves <ereeclimb@gmail.com>
2026-01-24 02:03:01 -0800

    add eric

--- nixosModules/minecraft-server.nix
+++ nixosModules/minecraft-server.nix
@@ -21,6 +21,7 @@
         knabster03 = "b96d9fd1-8551-4bae-8bd6-2589e4fc31f1";
         FlyingFish800 = "1b01dfa9-e9bc-432d-9063-33fc20616eb6";
         uiscupcake19 = "68a5d05a-1a39-4ea8-ba9c-4219498676b2";
+        ericbreh = "dc4e6ed0-55fe-41b7-b969-34894258c430";
       };
       serverProperties = {
         level-name = "world";
