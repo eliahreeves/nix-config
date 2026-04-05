5cf1f71cf288056fbae4860a57a12ea6669c9ecf
Eliah Reeves <ereeclimb@gmail.com>
2026-04-05 14:50:40 -0700

    cache

--- modules/hosts/nimh/configuration.nix
+++ modules/hosts/nimh/configuration.nix
@@ -26,9 +26,11 @@
     nix.settings = {
       extra-substituters = [
         "https://eko-network.cachix.org"
+        "https://nix-community.cachix.org"
       ];
       extra-trusted-public-keys = [
         "eko-network.cachix.org-1:1xHfovoNlydsTCzXxr5AstUoJUmGR/tRq0PQSCyPab8="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
       experimental-features = [
         "nix-command"
